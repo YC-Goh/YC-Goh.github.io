a3f6f2dde70d36d56d2f59c34e9d4c7ad37f125b
Goh Yeow Chong <gohyc1993@gmail.com>
2023-02-08 18:05:50 +0800

    fix: removed some unnecessary styling class tags and changed some styling options for the headers

--- components/header.tsx
+++ components/header.tsx
@@ -2,11 +2,11 @@
 import styles from './header.module.scss';
 
 export default function Header () {
-    return (<div className={`${styles['header']} container p-0`}>
-        <div className='row g-0 my-0 py-0'>
+    return (<div className={`${styles['header']} container`}>
+        <div className='row g-0'>
             <h2 className={`${styles['header-title']}`}>Hello World!</h2>
         </div>
-        <div className={`${styles['header-menu']} row g-0 m-0 p-0`}>
+        <div className={`${styles['header-menu']} row g-0`}>
             {menuLink('/', 'Home')}
             {menuLink('/', 'Projects')}
             {menuLink('/', 'TODO')}
@@ -15,7 +15,7 @@ export default function Header () {
 };
 
 function menuLink (href: string, label: string) {
-    return (<div className='col g-0 m-0 p-0'>
+    return (<div className='col g-0'>
         <a href={href} className={`${styles['header-menu-link']}`}><p className={`${styles['header-menu-link-label']}`}>{label}</p></a>
     </div>)
 };
