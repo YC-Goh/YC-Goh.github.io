82b407f24d307fb22c00199792b11924fedf1c1d
Goh Yeow Chong <gohyc1993@gmail.com>
2023-02-09 06:46:15 +0800

    feat: header and navigation bar are now fixed to top of viewport

--- components/header.tsx
+++ components/header.tsx
@@ -2,20 +2,22 @@
 import styles from './header.module.scss';
 
 export default function Header () {
-    return (<div className={`${styles['header']} container`}>
+    return (<div className={`${styles['header']} sticky-sm-top`}>
         <div className='row g-0'>
-            <h2 className={`${styles['header-title']}`}>Hello World!</h2>
+            <h1 className={`${styles['header-title']}`}>Hello World!</h1>
         </div>
-        <div className={`${styles['header-menu']} row g-0`}>
+        <nav className={`${styles['header-menu']} navbar row g-0`}>
             {menuLink('/', 'Home')}
             {menuLink('/', 'Projects')}
             {menuLink('/', 'TODO')}
-        </div>
+        </nav>
     </div>);
 };
 
 function menuLink (href: string, label: string) {
     return (<div className='col g-0'>
-        <a href={href} className={`${styles['header-menu-link']}`}><p className={`${styles['header-menu-link-label']}`}>{label}</p></a>
+        <a href={href} className={`${styles['header-menu-link']}`}>
+            <h4 className={`${styles['header-menu-link-label']}`}>{label}</h4>
+        </a>
     </div>)
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
 
 import Header from '../components/header';
 import style from './index.module.scss';
+import * as React from 'react';
 
 export default function App () {
-    return (<div className={style.app}>
+    let bodyText: React.ReactNode[] = [];
+    for (let i = 0; i < 1000; i++) {
+        bodyText.push(<p key={`key-${i}`}>Hello World</p>)
+    }
+    return (<div className={`${style['app']} container`}>
         <Header />
+        <div className={`row`}>
+            <div className={`col`}>
+                {bodyText}
+            </div>
+        </div>
     </div>)
 };
