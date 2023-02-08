56fab79f80c44ccab0f4c40878506f51171b0f1e
Goh Yeow Chong <gohyc1993@gmail.com>
2023-02-08 12:07:21 +0800

    feat: updated header to be static -- still unlinked -- links to be added after creating other pages

--- components/header.tsx
+++ components/header.tsx
@@ -3,6 +3,13 @@ import styles from './header.module.scss';
 
 export default function Header () {
     return (<div className={styles.header}>
-        <h2>Hello World</h2>
+        <h2 className={styles.title}>Hello World!</h2>
+        <div className={styles.menu}>
+            <a><p>Home</p></a>
+            <p>|</p>
+            <a><p>Projects</p></a>
+            <p>|</p>
+            <a><p>TODO</p></a>
+        </div>
     </div>);
 };
