32b925a0ce7fe3276076f6b9c64c63faa7b38b3b
Goh Yeow Chong <gohyc1993@gmail.com>
2023-02-09 11:06:15 +0800

    fix: styling for footer functional (but temporary -- to be reimplemented with font-awesome)

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,13 +1,15 @@
 
+import styles from './footer.module.scss';
+
 export default function Footer () {
-    return (<div>
-        <h2>Contact:</h2>
-        <div className='row g-0'>
-            <div className='col'>
-                <h4>LinkedIn</h4>
+    return (<div className={`${styles['footer']}`}>
+        <h2 className={`${styles['footer-text']} my-0`}>Contact:</h2>
+        <div className='row g-0 m-0 py-0'>
+            <div className='col m-0 py-0'>
+                <h4 className={`${styles['footer-text']} my-0`}>LinkedIn</h4>
             </div>
-            <div className='col'>
-                <h4>email</h4>
+            <div className='col m-0 py-0'>
+                <h4 className={`${styles['footer-text']} my-0`}>email</h4>
             </div>
         </div>
     </div>)
