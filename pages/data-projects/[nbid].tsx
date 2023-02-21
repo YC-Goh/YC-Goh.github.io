2ce304c61d6a6544cd1f44487010f39f63b59bed
Goh Yeow Chong <gohyc1993@gmail.com>
2023-02-21 16:00:46 +0800

    update: automatic construction of path list for getStaticPaths

--- pages/data-projects/[nbid].tsx
+++ pages/data-projects/[nbid].tsx
@@ -10,12 +10,14 @@ import fs from 'fs/promises';
 import path from 'path';
 import 'katex/dist/katex.css';
 
+let thisPublicPath = ['public', 'data-projects']
+
 export default function NoteBook (props: {nbContent: string}) {
     return (PageTemplate(Content(props.nbContent, 'markdown')));
 };
 
 export let getStaticProps: GetStaticProps<{nbContent:string}> = async (context) => {
-    let nbContent = await fs.readFile(path.resolve('public', 'data-projects', `${context.params!.nbid}.md`), {'encoding': 'utf8'})
+    let nbContent = await fs.readFile(path.resolve(...thisPublicPath, `${context.params!.nbid}.md`), {'encoding': 'utf8'})
     return {
         props: {
             nbContent
@@ -24,8 +26,15 @@ export let getStaticProps: GetStaticProps<{nbContent:string}> = async (context) 
 };
 
 export let getStaticPaths: GetStaticPaths = async () => {
+    let files = await fs.readdir(path.resolve(...thisPublicPath));
+    files = files.filter(function (file) {
+        return /.md$/i.test(file);
+    });
+    let filePaths = files.map(function (file) {
+        return { params: { nbid: file.replace(/.md$/i, '') } }
+    });
     return {
-        paths: [{ params: {nbid: 'sg-marriages-pam'} }],
+        paths: filePaths,
         fallback: false
     };
 };
