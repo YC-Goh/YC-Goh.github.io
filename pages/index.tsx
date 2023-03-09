f080f2222444f94b98d441977aeaccac92f01325
Goh Yeow Chong <gohyc1993@gmail.com>
2023-03-09 20:17:32 +0800

    update: main pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,16 +9,19 @@ export default function App (): JSX.Element {
 let contentMarkdown = `
 ## Home
 
-Details will be filled in later.
+Aspiring data scientist trained in the macro-econometrics tradition of empirical social science.
+- Profile: [LinkedIn](https://www.linkedin.com/in/yeow-chong-goh-3aab5818b/)
 
 ## Recently changed
 
-- I have configured the build steps to create pages from notebook files.
-  - [First example](/projects/sg-open-data/marriages-pam-educ).
-- Much of the site has been restyled using Bootstrap utilities.
+- Configured build steps to create pages from notebook files.
+  - [Example](/projects/sg-open-data/marriages-pam-educ).
+  - Going to redo this step soon because the current procedure cannot handle more complex \`MDX\` files.
+- Restyled using Bootstrap utilities.
 
 ## What else am I doing now
 
-- Aside from my current course (see LinkedIn): [HackerRank](https://www.hackerrank.com/gohyc1993), [LeetCode](https://leetcode.com/YC-Goh/), [freeCodeCamp](https://www.freecodecamp.org/yc-goh).
-  - Focus is on getting up to speed with data structures and algorithms.
+- [HackerRank](https://www.hackerrank.com/gohyc1993), [LeetCode](https://leetcode.com/YC-Goh/), [freeCodeCamp](https://www.freecodecamp.org/yc-goh).
+  - Focus is on databases, data structures and algorithms.
+  - Recently finished: basic data structures (LeetCode), basic SQL (LeetCode \& HackerRank), intermediate SQL (HackerRank).
 `;
