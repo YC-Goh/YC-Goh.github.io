33321e44e0804f5bf2a988663ad7a68e63ee4f7f
Goh Yeow Chong <gohyc1993@gmail.com>
2023-03-19 19:41:21 +0800

    update: front page

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,8 +22,15 @@ Aspiring data scientist trained in the macro-econometrics tradition of empirical
 ## What else am I doing now
 
 - [HackerRank](https://www.hackerrank.com/gohyc1993), [LeetCode](https://leetcode.com/YC-Goh/), [freeCodeCamp](https://www.freecodecamp.org/yc-goh).
-  - Focus is on databases, data structures and algorithms.
-  - Recently finished: basic data structures (LeetCode), basic SQL (LeetCode \& HackerRank), intermediate SQL (HackerRank).
+  - Doing: Databases (HackerRank)
+  - Done: basic data structures (LeetCode), basic algorithms (LeetCode), basic SQL (LeetCode/HackerRank), intermediate SQL (HackerRank).
+- Doing the [SQL tutorial](https://mode.com/sql-tutorial/introduction-to-sql/) on Mode Analytics.
+  - Course requirement in this DS/AI course.
+  - Forgot to do each example exercise in a separate query (even though it is pointless: the answers are also given and public).
+- Doing the [Data Science in Python Boot Camp](https://www.udemy.com/course/the-data-science-course-complete-data-science-bootcamp/) on Udemy.
+  - As an additional supplement to the DS/AI course.
+  - For the certification as well to show on LinkedIn.
+  - Because it is free via the [National Library Board](https://eresources.nlb.gov.sg/main) subscription to Udemy.
 - Playing with [Tableau](https://public.tableau.com/app/profile/goh.yeow.chong).
   - Started because of some assignments in this DS/AI course but going to just do some of my own because why not.
 `;
