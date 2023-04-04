19fd0178892708531a8b752f8dc01c925067c789
Goh Yeow Chong <gohyc1993@gmail.com>
2023-04-04 13:43:44 +0800

    update: front page

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,6 +28,7 @@ Aspiring data scientist coming from the more traditional econometrics way of thi
 - Doing the [SQL tutorial](https://mode.com/sql-tutorial/introduction-to-sql/) on Mode Analytics.
   - Course requirement in this DS/AI course.
   - Forgot to do each example exercise in a separate query (even though it is pointless: the answers are also given and public).
+- Doing the [Neo4j tutorial](https://graphacademy.neo4j.com/u/5bbf4d76-7300-4c0f-9adf-3b389d478109/).
 - Doing the [Data Science in Python Boot Camp](https://www.udemy.com/course/the-data-science-course-complete-data-science-bootcamp/) on Udemy.
   - As an additional supplement to the DS/AI course.
   - For the certification as well to show on LinkedIn.
