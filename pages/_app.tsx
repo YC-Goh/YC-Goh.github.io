7a786a0546d2d29927dfcf7e2668e10fc127b8d1
Goh Yeow Chong <gohyc1993@gmail.com>
2023-09-24 12:56:37 +0800

    Remove all content for website revamp

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-
-import './next.scss';
-import 'bootstrap/scss/bootstrap-grid.scss';
-import 'bootstrap/scss/bootstrap-utilities.scss';
-import {ComponentType} from 'react';
-
-export default function App ({Component, pageProps} : {Component: ComponentType, pageProps: {}}) {
-    return (<Component {...pageProps} />);
-};
