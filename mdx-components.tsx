5c9ccb2535c1d6f8d57dc53f7a019a9c24c831c3
Goh Yeow Chong <gohyc1993@gmail.com>
2024-05-22 10:44:54 +0800

    Update: Push all outstanding to main to restart dev-main procedure

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -3,8 +3,8 @@ import type { MDXComponents } from 'mdx/types'
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     h1: ({ children }) => <h1 className="text-5xl py-2">{ children }</h1>,
-    h2: ({ children }) => <h2 className="text-lg py-2">{ children }</h2>,
-    h3: ({ children }) => <h3 className="text-md py-2">{ children }</h3>,
+    h2: ({ children }) => <h2 className="text-xl py-2">{ children }</h2>,
+    h3: ({ children }) => <h3 className="text-lg py-2">{ children }</h3>,
     p: ({ children }) => <p className="text-xs">{ children }</p>,
     a: (props) => <a className="hover:text-slate-500" {...props}/>,
     ol: ({ children }) => <ol className="list-decimal px-4 py-2">{ children }</ol>,
