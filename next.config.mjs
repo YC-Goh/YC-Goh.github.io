eb40c947746a69d14a6174232872b85ea0784987
Goh Yeow Chong <gohyc1993@gmail.com>
2025-06-07 18:31:21 +0800

    Update: Update starter files under weekly

--- next.config.mjs
+++ next.config.mjs
@@ -2,21 +2,21 @@
 import createMDX from '@next/mdx'
 
 export default (phase, { defaultConfig }) => {
-  /**
-   * @type {import('next').NextConfig}
-   */
-  const nextConfig = {
-    // Configure `pageExtensions` to include markdown and MDX files
-    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'], 
-    /* config options here */
-    output: 'export', 
-    basePath: process.env.PAGES_BASE_PATH, 
-  }
+    /**
+     * @type {import('next').NextConfig}
+     */
+    const nextConfig = {
+        // Configure `pageExtensions` to include markdown and MDX files
+        pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'], 
+        /* config options here */
+        output: 'export', 
+        basePath: process.env.PAGES_BASE_PATH, 
+    }
 
-  const withMDX = createMDX({
-    extension: /\.(md|mdx)$/,
-    // Add markdown plugins here, as desired
-  })
+    const withMDX = createMDX({
+        extension: /\.(md|mdx)$/,
+        // Add markdown plugins here, as desired
+    })
 
-  return withMDX(nextConfig)
+    return withMDX(nextConfig)
 }
