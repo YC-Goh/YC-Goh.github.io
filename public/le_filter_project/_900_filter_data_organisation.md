696bc2640482b59d25afe0a4175365cebc36f79e
Goh Yeow Chong <gohyc1993@gmail.com>
2025-09-14 12:04:26 +0800

    Update: Move things around

---
+++ public/le_filter_project/_900_filter_data_organisation.md
@@ -0,0 +1,77 @@
+Folder structure description format:
+1.  Folders: folder_name [source]
+    1.  Note: source is only listed for folders directly containing data files
+2.  Files: file_name.ext [schema]
+    1.  Schema for JSON, HTML, XML files:
+        1.  Type 1: {level_0_0 (type): value, level_0_1 (type): value, ...}
+        2.  Type 2: [{level_0_0 (type): value, level_0_1 (type): value, ...}]
+            -   [{...}] implies a list of {...}
+        3.  value takes either Type 1 or Type 2 for nested JSON, or a single value or [list of values] for final values.
+    2.  Schema for CSV files: col1 (type), col2 (type), col3 (type), ..., colN (type)
+    3.  Note: [file_name1-file_name2] and [file_name1/file_name2/...] indicate range of files sharing same schema. 
+    4.  Note: some schemas may not yet be filled out
+    5.  Note: possible types are str, int, dbl, bool
+
+Folder structure:
+-   affix_id
+    -   combined [data pipeline output]
+    -   details
+        -   processed [data pipeline output]
+            -   [prefix/suffix].csv [Name (str), Type (str), Applies To (str)]
+            -   [prefix/suffix].json
+        -   raw [https://www.lastepochtools.com/db/]
+            -   [prefix/suffix].html
+    -   game_data
+        -   processed [manual processing output]
+            -   affix_id.csv [affix_id (int), affix_class (str), affix_group (str), affix_name (str)]
+        -   raw [in-game data scraping]
+            -   affix_id.xml
+    -   idol_tiers
+        -   processed [data pipeline output]
+            -   [01-05].csv [Affix Type (str), Name (str), Reroll % (str), Tier1 (str), Tier2 (str), Tier3 (str), Tier4 (str), Tier5 (str), Tier6 (str), Tier7 (str), Source File (str)]
+            -   [01-05].json
+            -   data.csv [Affix Type (str), Name (str), Reroll % (str), Tier1 (str), Tier2 (str), Tier3 (str), Tier4 (str), Tier5 (str), Tier6 (str), Tier7 (str), Source File (str)]
+            -   data.json
+        -   raw [https://lastepoch.tunklab.com/affixes]
+            -   [01-05].html
+    -   item_tiers
+        -   processed [data pipeline output]
+            -   [01-05].csv [Affix Type (str), Lvl (int), Name (str), Reroll % (str), Tier1 (str), Tier2 (str), Tier3 (str), Tier4 (str), Tier5 (str), Tier6 (str), Tier7 (str), Tier8 (str), Source File (str)]
+            -   [01-05].json
+            -   data.csv [Affix Type (str), Lvl (int), Name (str), Reroll % (str), Tier1 (str), Tier2 (str), Tier3 (str), Tier4 (str), Tier5 (str), Tier6 (str), Tier7 (str), Tier8 (str), Source File (str)]
+            -   data.json
+        -   raw [https://lastepoch.tunklab.com/affixes]
+            -   [01-05].html
+-   condition_list
+    -   processed [data pipeline output]
+    -   raw [in-game data scraping]
+        -   condition_list.xml
+-   item_id
+    -   combined [data pipeline output]
+    -   details
+        -   processed [data pipeline output]
+        -   raw [in-game data scraping]
+            -   [1h_axe/1h_dagger/1h_mace/1h_sceptre/1h_sword/1h_wand/2h_axe/2h_bow/2h_mace/2h_spear/2h_staff/2h_sword/amulet/armour/belt/boots/catalyst/gloves/helmet/quiver/relic/ring/shield/type].html
+    -   game_data
+        -   processed [manual processing output]
+        -   raw [in-game data scraping]
+-   unique_id
+    -   combined [data pipeline output]
+    -   details
+        -   processed [data pipeline output]
+            -   all.json
+        -   raw [https://www.lastepochtools.com/db/]
+            -   all.html
+    -   game_data
+        -   processed [data pipeline output]
+        -   raw [in-game data scraping]
+    -   lp_tiers
+        -   processed [data pipeline output]
+        -   raw [https://lastepoch.tunklab.com/uniques]
+            -   [01-05].html
+-   _011_affix_tiers_table_parser.py
+-   _012_affix_tiers_file_merger.py
+-   _013_affix_tiers_batch_processor.py
+-   _021_affix_details_table_parser.py
+-   _022_affix_details_json_parser.py
+-   _031_unique_details_table_parser.py
--- public/marriage_divorce_birth/core.py
+++ public/marriage_divorce_birth/core.py
@@ -1,8 +1,137 @@
 
 #%%
 
-import os, sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)))
-from public.core import *
+import os
+import pandas as pd
+from IPython.display import display
+import matplotlib.pyplot as plt
+import matplotlib.axes as axes
+import seaborn as sns
+
+#%%
+
+project_directories = {'root': os.path.abspath(os.path.dirname(__file__))}
+project_directories['root_marriage'] = os.path.join(project_directories['root'], 'marriage_divorce_birth')
+
+for key, path in list(project_directories.items()):
+    if key.startswith('root_'):
+        for subfold in ['raw', 'out', 'compiled']:
+            subpath = os.path.join(path, subfold)
+            if os.path.exists(subpath):
+                subkey = key.replace('root_', f'{subfold}_', 1)
+                project_directories[subkey] = subpath
+
+__error_paths = {key: path for key, path in project_directories.items() if not os.path.exists(path)}
+assert len(__error_paths) == 0, f'Following paths do not exist: {__error_paths}'
+
+#%%
+
+def display_all_columns(data:pd.DataFrame)->None:
+    with pd.option_context('display.max_columns', None):
+        display(data)
+
+#%%
+
+def display_all_rows(data:pd.DataFrame)->None:
+    with pd.option_context('display.max_rows', None):
+        display(data)
+
+#%%
+
+def display_full_table(data:pd.DataFrame)->None:
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+        display(data)
+
+#%%
+
+def plot_singlecol_data(data:pd.DataFrame, ylabel:str, splitplot:bool=True)->tuple[plt.Figure,axes.Axes]:
+    y_levels = data.columns.unique()
+    if splitplot:
+        n_y = y_levels.nunique()
+    else:
+        n_y = 1
+    h = 1 * 3
+    w = n_y * (3 * (3 / 2))
+    fig, axs = plt.subplots(1, n_y, figsize=(w, h), sharex=True, sharey=True)
+    if splitplot:
+        axs[0].set_ylabel(ylabel)
+    else:
+        axs:axes.Axes
+        axs.set_ylabel(ylabel)
+    if splitplot:
+        for y in y_levels:
+            i_y = y_levels.get_indexer_for([y])[0]
+            ax_y:axes.Axes = axs[i_y]
+            ax_y.set_facecolor(('white', 0.5))
+            ax_y.set_title(y)
+            try:
+                sns.lineplot(data.loc[:,y], ax=ax_y, c=sns.color_palette('Dark2')[0])
+            except:
+                pass
+    else:
+        axs.set_facecolor(('white', 0.5))
+        try:
+            sns.lineplot(data, ax=axs, dashes=False, palette='Dark2', markers=True)
+            h, l = axs.get_legend_handles_labels()
+            t = axs.get_legend().get_title().get_text()
+            axs.legend(h, l, fontsize='xx-small', title=t, title_fontsize='xx-small')
+        except:
+            pass
+    fig.get_tight_layout()
+    fig.set_facecolor(('white', 0.5))
+    return fig, axs
+
+#%%
+
+def plot_multicol_data(data:pd.DataFrame, x_level:str, y_level:str)->tuple[plt.Figure,axes.Axes]:
+    x_levels = data.columns.unique(x_level)
+    y_levels = data.columns.unique(y_level)
+    n_x = x_levels.nunique()
+    n_y = y_levels.nunique()
+    if n_y == 1:
+        h = n_y * 3
+        w = n_x * (3 * (3 / 2))
+        fig, axs = plt.subplots(n_y, n_x, figsize=(w, h), sharex=True, sharey=True)
+    else:
+        h = n_x * 3
+        w = n_y * (3 * (3 / 2))
+        fig, axs = plt.subplots(n_x, n_y, figsize=(w, h), sharex=True, sharey=True)
+    for x in x_levels:
+        i_x = x_levels.get_indexer_for([x])[0]
+        if n_y > 1:
+            axs[i_x][0].set_ylabel(f'{x_level.title()}: {x}')
+        else:
+            axs[i_x].set_title(f'{x_level.title()}: {x}')
+        for y in y_levels:
+            i_y = y_levels.get_indexer_for([y])[0]
+            if n_y > 1:
+                ax_xy:axes.Axes = axs[i_x][i_y]
+            else:
+                ax_xy:axes.Axes = axs[i_x]
+            ax_xy.set_facecolor(('white', 0.5))
+            if i_x == 0:
+                if n_y > 1:
+                    ax_xy.set_title(f'{y_level.title()}: {y}')
+                else:
+                    ax_xy.set_ylabel(f'{y_level.title()}: {y}')
+            if n_y == 1 or i_x == n_x - 1:
+                ax_xy.tick_params(axis='x', labelrotation=45)
+            try:
+                plot_data = data.xs((x,y,), 1, (x_level,y_level,)).copy(deep=True)
+                if plot_data.columns.nlevels == 1:
+                    if plot_data.columns.size == 1:
+                        plot_data = plot_data.iloc[:,[0]]
+                    sns.lineplot(plot_data, ax=ax_xy, dashes=False, palette='Dark2', markers=True)
+                    h, l = ax_xy.get_legend_handles_labels()
+                    t = ax_xy.get_legend().get_title().get_text()
+                    ax_xy.legend(h, l, fontsize='xx-small', title=t, title_fontsize='xx-small')
+                else:
+                    plot_data = plot_data.iloc[:,0]
+                    sns.lineplot(plot_data, ax=ax_xy, c=sns.color_palette('Dark2')[0])
+            except:
+                pass
+    fig.get_tight_layout()
+    fig.set_facecolor(('white', 0.5))
+    return fig, axs
 
 #%%
